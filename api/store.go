f9b01d07c40ca523921a34c6976a39f1ffa92991
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2015-11-05 14:55:59 +0000

    Add db port conn

--- api/store.go
+++ api/store.go
@@ -10,7 +10,7 @@ import (
 func GetConn() gorm.DB {
   log.Println("Starting database connection")
   
-  db, err := gorm.Open("mysql", "user01:fqxjXS6fh5eK3b64@tcp(104.197.132.233)/user01?charset=utf8&parseTime=True&loc=Local"); if err != nil {
+  db, err := gorm.Open("mysql", "user01:fqxjXS6fh5eK3b64@tcp(104.197.132.233:3306)/user01?charset=utf8"); if err != nil {
     log.Fatalln(err)
   }
   defer db.Close()
